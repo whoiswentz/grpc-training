4c56d6c420ccc7b69d6fbc699217f70f83111c8b
Vinicios Wentz <wentz.vinicios@gmail.com>
2019-07-05 17:01:33 -0300

    Read RPC Implemented

--- blog/blog_client/main.go
+++ blog/blog_client/main.go
@@ -22,15 +22,33 @@ func main() {
 
 	client := blogpb.NewBlogServiceClient(clientConn)
 
+	createdBlog := CreateBlog(client)
+	log.Println(createdBlog)
+
+	readedBlog := ReadBlog(createdBlog, client)
+	log.Println(readedBlog)
+}
+
+func CreateBlog(client blogpb.BlogServiceClient) *blogpb.CreateBlogResponse {
 	blog := &blogpb.Blog{
 		AuthorId: "Vinicios Wentz",
 		Title:    "gRPC Awesomeness",
 		Content:  "A lot of text about gRPC",
 	}
-
 	response, err := client.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{Blog: blog})
 	if err != nil {
 		log.Fatalf("%v\n", err)
 	}
-	log.Println(response)
+	return response
+}
+
+func ReadBlog(response *blogpb.CreateBlogResponse, client blogpb.BlogServiceClient) interface{} {
+	blogId := response.GetBlog().GetId()
+
+	readBLogRequest := &blogpb.ReadBlogRequest{BlogId: blogId}
+	readBlogResponse, err := client.ReadBlog(context.Background(), readBLogRequest)
+	if err != nil {
+		log.Fatalf("%v\n", err)
+	}
+	return readBlogResponse
 }
--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -3,11 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"grpc-training/blog/blogpb"
 	"grpc-training/blog/model"
+	"log"
 )
 
 type BlogServer struct {
@@ -15,7 +17,7 @@ type BlogServer struct {
 	Address string
 }
 
-func (BlogServer) CreateBlog(context context.Context, request *blogpb.CreateBlogRequest) (*blogpb.CreateBlogResponse, error) {
+func (*BlogServer) CreateBlog(context context.Context, request *blogpb.CreateBlogRequest) (*blogpb.CreateBlogResponse, error) {
 	blog := request.GetBlog()
 
 	blogItem := model.BlogItem{
@@ -44,4 +46,28 @@ func (BlogServer) CreateBlog(context context.Context, request *blogpb.CreateBlog
 	}
 
 	return blogResponse, nil
+}
+
+func (*BlogServer) ReadBlog(context context.Context, request *blogpb.ReadBlogRequest) (*blogpb.ReadBlogResponse, error) {
+	log.Println("Reading blog request")
+
+	blogID, err := primitive.ObjectIDFromHex(request.GetBlogId())
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, fmt.Sprintf("Cannot parse ID"))
+	}
+
+	blogItem := &model.BlogItem{}
+	result := collection.FindOne(context, bson.M{"_id": blogID})
+	if err := result.Decode(blogItem); err != nil {
+		return nil, status.Errorf(codes.NotFound, fmt.Sprintf("Cannot find blog with specified ID: %v\n", err))
+	}
+
+	return &blogpb.ReadBlogResponse{
+		Blog: &blogpb.Blog{
+			Id:       blogItem.ID.Hex(),
+			AuthorId: blogItem.AuthorID,
+			Title:    blogItem.Title,
+			Content:  blogItem.Content,
+		},
+	}, nil
 }
